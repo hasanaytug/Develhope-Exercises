24788e20988fe5d61a06f1b168c7f72749483a67
Hasan <hasan.aytug@gmail.com>
2023-01-13 15:57:13 +0300

    components-7

--- React/components/src/Hello.js
+++ React/components/src/Hello.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Message from "./Message";
 
 class Hello extends React.Component {
   render() {
     return (
       <>
         <h1>Hello, World!</h1>
-        {/* I saved this as a branch 5 but there is no exercises 5 so this one is exercise 6 */}
-        <p>What a beautiful day!</p>
+        <Message />
       </>
     );
   }
