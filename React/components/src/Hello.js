ec8f0164e027e457b3f38958c07d3ca93f3e0556
Hasan <hasan.aytug@gmail.com>
2023-01-13 15:46:19 +0300

    note added

--- React/components/src/Hello.js
+++ React/components/src/Hello.js
@@ -5,7 +5,7 @@ class Hello extends React.Component {
     return (
       <>
         <h1>Hello, World!</h1>
-        {//I saved this as a branch 5 but there is no exercises 5 so this one is exercise 6}
+        {/* I saved this as a branch 5 but there is no exercises 5 so this one is exercise 6 */}
         <p>What a beautiful day!</p>
       </>
     );
