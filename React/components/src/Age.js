8eacb596ccb24074b2ba41a16f17baf7360ffe18
Hasan <hasan.aytug@gmail.com>
2023-01-15 16:05:02 +0300

    conditional rendering 5

--- React/components/src/Age.js
+++ React/components/src/Age.js
@@ -4,7 +4,11 @@ class Age extends React.Component {
   render() {
     return (
       <>
-        <p>Your age is {this.props.age}</p>
+        {this.props.age > 18 ? (
+          <p>Your age is {this.props.age}</p>
+        ) : (
+          <p>Your are too young</p>
+        )}
       </>
     );
   }
--- React/components/src/Welcome.js
+++ React/components/src/Welcome.js
@@ -6,11 +6,7 @@ class Welcome extends React.Component {
     return (
       <>
         <p>Welcome {this.props.name}!</p>
-        {this.props.age > 18 &&
-        this.props.age < 65 &&
-        this.props.name === "John" ? (
-          <Age age={this.props.age} />
-        ) : null}
+        <Age age={this.props.age} />
       </>
     );
   }
