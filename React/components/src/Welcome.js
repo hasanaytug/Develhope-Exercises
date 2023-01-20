128638a6e97b1682cc786acf9ee17f7d047045e0
Hasan <hasan.aytug@gmail.com>
2023-01-20 14:28:50 +0300

    styling-1

--- React/components/src/Welcome.js
+++ React/components/src/Welcome.js
@@ -4,10 +4,10 @@ import Age from "./Age";
 class Welcome extends React.Component {
   render() {
     return (
-      <>
+      <div className="welcome">
         <p>Welcome {this.props.name}!</p>
         <Age age={this.props.age} />
-      </>
+      </div>
     );
   }
 }
