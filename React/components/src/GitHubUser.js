11a816cb706acaa344c74cdb2e82758b5a48d005
Hasan <hasan.aytug@gmail.com>
2023-01-22 20:26:51 +0300

    custom-components-3

--- React/components/src/GitHubUser.js
+++ React/components/src/GitHubUser.js
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from "react";
 
 function GitHubUser({ username }) {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  function useGithubUser(username) {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState("Loading...");
 
-  async function fetchUserData(username) {
-    try {
-      setError(null);
-      const responce = await fetch(`https://api.github.com/users/${username}`);
-      if (responce.status !== 200) {
-        setError("Error");
+    async function fetchUserData(username) {
+      try {
+        setError(null);
+        const responce = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        if (responce.status !== 200) {
+          setError("Error");
+          setUser(null);
+        }
+        const json = await responce.json();
+
+        setUser(json);
+        setLoading(null);
+      } catch (err) {
+        setError(err);
         setUser(null);
       }
-      const json = await responce.json();
-
-      setUser(json);
-    } catch (err) {
-      setError(err);
-      setUser(null);
     }
+    useEffect(() => {
+      fetchUserData(username);
+    }, [username]);
+
+    return {
+      user: user,
+      error: error,
+      loading,
+      loading,
+    };
   }
-  useEffect(() => {
-    fetchUserData(username);
-  }, [username]);
+
+  const { user, error, loading } = useGithubUser(username);
 
   return (
     <div>
+      {loading && <h1>{loading}</h1>}
       {user && <h1>{user.login}</h1>}
       {error && <h1>Something went wrong</h1>}
     </div>
